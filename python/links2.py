c4f4ff5220493ece3f0687395d1457b4e0d22e99
socialsb <gert.vaikre@gmail.com>
2014-04-21 19:30:07 +0300

    lisatud kontrollid ja kommentaarid

--- python/links2.py
+++ python/links2.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Võtab sisendist URLi ja leiab sealt lehelt kõik lingid ning prindib need
+# välja.
+#
+# Väljundi faili saamiseks võib kasutada suunamist.
+#
 # Gert Vaikre A21
 #
 # http://www.pythonforbeginners.com/code/regular-expression-re-findall
 #
-from __future__ import print_function
-import sys
 
+import sys
 import re
 import urllib2
-    
-website = urllib2.urlopen(sys.argv[1])
 
-html = website.read()
-    
-links = re.findall('<a href=(.*?)[ >]', html)
-file=open('lingid.txt', 'w')
+if len(sys.argv) != 2:                                                          
+    print "Argumentide arv on vale!"                                            
+    print "Kasuta programmi", sys.argv[0], "URL"                         
+    sys.exit(1)
 
-for link in links:
-    file.write(link)
-    file.write("\n")
+aadress = sys.argv[1]
+
+try:    
+    veebileht = urllib2.urlopen(aadress)
+except ValueError:
+    print "URL ei ole õigel kujul."
+    print "Kasutamise näide:"
+    print sys.argv[0], "http://www.itcollege.ee"
+    sys.exit(2)
+
+html = veebileht.read()
+lingid = re.findall('<a href=(.*?)[ >]', html)
+
+for link in lingid:
+    print link.replace('"', '')
